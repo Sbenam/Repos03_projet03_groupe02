03e92535cabcbedf4e1d232e29a35fae331e5563
FilouDeNesque <johndoe@example.com>
2021-08-10 18:10:53 +0200

    inscripton 1 regle de gestion

--- AppTontaPatWeb/src/main/java/fr/eql/ai109/apptontapat/controller/AccountManagedBean.java
+++ AppTontaPatWeb/src/main/java/fr/eql/ai109/apptontapat/controller/AccountManagedBean.java
@@ -1,12 +1,13 @@
 package fr.eql.ai109.apptontapat.controller;
 
 import java.io.Serializable;
-import java.lang.System.Logger;
+import java.util.regex.Pattern;
 
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
 import fr.eql.ai109.apptontapat.entity.Account;
@@ -14,7 +15,7 @@ import fr.eql.ai109.apptontapat.entity.ZipCode;
 import fr.eql.ai109.apptontapat.ibusiness.AccountIBusiness;
 import fr.eql.ai109.apptontapat.web.PageManageBean;
 
-@ManagedBean(name = "mbAccount")
+@ManagedBean(name = "mbAccount", eager = true)
 @SessionScoped
 public class AccountManagedBean implements Serializable {
 
@@ -31,16 +32,15 @@ public class AccountManagedBean implements Serializable {
 	private ZipCode zipCode;
 	private Account account;
 
-
 	@EJB
 	private AccountIBusiness accountIBusiness;
 
 	public void inscription(Account account) {
-		setAccount( (accountIBusiness.inscription(account) != null) ? account : null);
+		setAccount((accountIBusiness.inscription(account) != null) ? account : null);
 	}
 
 	public String connect() {
-		//TODO sysout Account
+		// TODO sysout Account
 		System.err.println("Passage dans la function connect");
 		System.err.println("email : " + email + " password: " + password);
 		String forward = null;
@@ -99,10 +99,12 @@ public class AccountManagedBean implements Serializable {
 	}
 
 	public String getEmail() {
+		System.out.println("email getter: " + email);
 		return email;
 	}
 
 	public void setEmail(String email) {
+		System.out.println("email setter: " + email);
 		this.email = email;
 	}
 
@@ -115,27 +117,53 @@ public class AccountManagedBean implements Serializable {
 	}
 
 	public String getPasswordVerif() {
-		System.out.println(passwordVerif);
+		System.out.println("getter passwordVerif" + passwordVerif);
 		return passwordVerif;
 	}
-	
+
 	public void setPasswordVerif(String passwordVerif) {
-		System.out.println(passwordVerif);
+		System.out.println("setter passwordverif: " + passwordVerif);
 		this.passwordVerif = passwordVerif;
 	}
+
 	public String getCgu() {
-		System.out.println(cgu);
+		System.out.println("getter cgu: " + cgu);
 		return cgu;
 	}
-	
+
 	public void setCgu(String cgu) {
-		System.out.println(cgu);
+		System.out.println("setter cgu : " + cgu);
 		this.cgu = cgu;
 	}
 
-	public void verifmail(){
-		PageManageBean managedBean = new PageManageBean();
-		managedBean.setPage2("inscription2");
+	public void verifmail(PageManageBean managedBean) {
+		System.out.println("verifmail function (email): " +email);
+		if (email != null) {
+			
+			boolean RMailV = Pattern.matches(
+					"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", email);
+			boolean Rpassword = Pattern.matches(
+					"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", email);
+			if (RMailV && Rpassword ) {
+				System.out.println("verif mail " + RMailV);
+				managedBean.setPage2("inscription2");
+			} elseif (Rmail) {
+				managedBean.setPage2("inscription1");
+				FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_WARN,
+					"Identifiant et/ou mot de passe incorrect(s)", "Email incorrect");
+			FacesContext.getCurrentInstance().addMessage("loginForm:inpPassword", facesMessage);
+			System.out.println("Erreur de connexion");
+				System.out.println("verif mail " + RMailV);
+			} elseif (Rpassword) {
+				managedBean.setPage2("inscription1");
+				FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_WARN,
+					"Identifiant et/ou mot de passe incorrect(s)", "mot de passe incorrect(s)");
+			FacesContext.getCurrentInstance().addMessage("loginForm:password-input", facesMessage);
+			System.out.println("Erreur de connexion");
+				System.out.println("verif passeword " + Rpassword);
+			}
+		}
+		System.out.println("-------------------------------------------");
 	}
 
 }
