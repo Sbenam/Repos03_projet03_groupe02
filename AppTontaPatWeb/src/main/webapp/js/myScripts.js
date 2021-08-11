ad8e1d10f57156135bcd8fe5be3e03eac466012c
FilouDeNesque <johndoe@example.com>
2021-08-12 00:37:26 +0200

    modification welcom page ajout de la carte modification css

--- AppTontaPatWeb/src/main/webapp/js/myScripts.js
+++ AppTontaPatWeb/src/main/webapp/js/myScripts.js
@@ -23,3 +23,19 @@ function informations3(){
     new bootstrap.Tooltip(tooltipTriggerEl)
   })
 })()
+
+
+
+
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+// https://api.mapbox.com/styles/v1/pcuquemy/cks827ivm1pfh18s8prk9qzwv/wmts?access_token=pk.eyJ1IjoicGN1cXVlbXkiLCJhIjoiY2txNW82c3hlMGQxODJ2cW80a20wdXVhaCJ9.Xa-faAijdGvYFWDsKC-Zzw
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'pcuquemy/cks827ivm1pfh18s8prk9qzwv',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoicGN1cXVlbXkiLCJhIjoiY2txNW82c3hlMGQxODJ2cW80a20wdXVhaCJ9.Xa-faAijdGvYFWDsKC-Zzw'
+}).addTo(mymap);
