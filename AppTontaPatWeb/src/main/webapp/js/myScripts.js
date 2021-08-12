67cf77d4607654630a3c7af2c51e10be66918bfa
FilouDeNesque <johndoe@example.com>
2021-08-12 18:24:54 +0200

    Ajout des markers sur la carte dans welcome page

--- AppTontaPatWeb/src/main/webapp/js/myScripts.js
+++ AppTontaPatWeb/src/main/webapp/js/myScripts.js
@@ -27,7 +27,44 @@ function informations3(){
 
 
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+
+var greenIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+var redIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+var sheepIcon = new L.Icon({
+  iconUrl: "https://img.icons8.com/cotton/64/000000/sheep2--v2.png",
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [32, 32],
+  iconAnchor: [16, 16],
+  popupAnchor: [1, -16],
+  shadowSize: [41, 41]
+});
+var fieldIcon = new L.Icon({
+  iconUrl: "https://img.icons8.com/material-two-tone/48/000000/house-with-a-garden.png",
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [32, 32],
+  iconAnchor: [16, 16],
+  popupAnchor: [1, -16],
+  shadowSize: [41, 41]
+});
+
+
+var mymap = L.map('mapid').setView([44.4690744788 , 3.49143883359], 13);
 
 // https://api.mapbox.com/styles/v1/pcuquemy/cks827ivm1pfh18s8prk9qzwv/wmts?access_token=pk.eyJ1IjoicGN1cXVlbXkiLCJhIjoiY2txNW82c3hlMGQxODJ2cW80a20wdXVhaCJ9.Xa-faAijdGvYFWDsKC-Zzw
 
@@ -39,3 +76,26 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoicGN1cXVlbXkiLCJhIjoiY2txNW82c3hlMGQxODJ2cW80a20wdXVhaCJ9.Xa-faAijdGvYFWDsKC-Zzw'
 }).addTo(mymap);
+
+var latitudeOffre = document.getElementsByClassName("herdlong");
+var longitudeOffre = document.getElementsByClassName("herdlat");
+
+console.log("latitude length: "+ latitudeOffre.length)
+for (var i = 0; i <latitudeOffre.length; i++){
+  console.log("latitude: "+ latitudeOffre[i].value)
+  platitudeOffre = parseFloat(latitudeOffre[i].value.replace(',', '.'))
+  plongitudeOffre = parseFloat(longitudeOffre[i].value.replace(',', '.'))
+  marcker = new L.marker([platitudeOffre, plongitudeOffre], { icon: sheepIcon }).bindPopup('Troupeau').addTo(mymap);
+}
+
+var latitudeTerrain = document.getElementsByClassName("fieldlong");
+var longitudeTerrain = document.getElementsByClassName("fieldlat");
+
+console.log("latitude length: "+ latitudeTerrain.length)
+
+for (var i = 0; i <latitudeTerrain.length; i++){
+  console.log("latitude: "+ latitudeTerrain[i].value)
+  plattitudeTerrain = parseFloat(latitudeTerrain[i].value.replace(',', '.'))
+  plongitudeTerrain = parseFloat(longitudeTerrain[i].value.replace(',', '.'))
+  marcker = new L.marker([plattitudeTerrain, plongitudeTerrain], { icon: fieldIcon }).bindPopup('Terrain').addTo(mymap);
+}
