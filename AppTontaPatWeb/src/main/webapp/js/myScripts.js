e6188b5b6cfb1bca31e97fbff3931dfbbc6b6c04
Your Name <you@example.com>
2021-08-12 16:14:38 +0200

    suppression page field

--- AppTontaPatWeb/src/main/webapp/js/myScripts.js
+++ AppTontaPatWeb/src/main/webapp/js/myScripts.js
@@ -1,3 +1,30 @@
+$(document).ready(function(){
+	
+	
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+
+
+});
+
+
+
 
 function connect(){
     console.log("test")
