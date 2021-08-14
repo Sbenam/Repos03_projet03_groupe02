917a09c7433a2a87240952a0d53e29c999c4369f
FilouDeNesque <johndoe@example.com>
2021-08-14 23:26:00 +0200

    ajout des liaison terrain modifier/ details

--- AppTontaPatWeb/src/main/java/fr/eql/ai109/apptontapat/controller/FieldManagedBean.java
+++ AppTontaPatWeb/src/main/java/fr/eql/ai109/apptontapat/controller/FieldManagedBean.java
@@ -23,34 +23,36 @@ import fr.eql.ai109.apptontapat.ibusiness.RetretIBusiness;
 import fr.eql.ai109.apptontapat.ibusiness.SlopeIbusiness;
 import fr.eql.ai109.apptontapat.ibusiness.VegetationIBusiness;
 import fr.eql.ai109.apptontapat.ibusiness.ZipCodeIBusiness;
+import fr.eql.ai109.apptontapat.web.PageManageBean;
 
 @ManagedBean(name = "mbField")
 @SessionScoped
-public class FieldManagedBean implements Serializable{
+public class FieldManagedBean implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
+	private List<Field> allField = new ArrayList<Field>();
+	private Field selectField = new Field();
 	private Field field = new Field();
 	private List<String> slopes = new ArrayList<String>();
-	private String selectedSlope ;
+	private String selectedSlope;
 	private List<String> grasses = new ArrayList<String>();
-	private String selectedGrass ;
+	private String selectedGrass;
 	private List<String> humidities = new ArrayList<String>();
-	private String selectedHumidity ;
+	private String selectedHumidity;
 	private List<String> Veggies = new ArrayList<String>();
-	private String selectedVeggy ;
+	private String selectedVeggy;
 	private List<String> compos = new ArrayList<String>();
-	private String selectedCompo ;
+	private String selectedCompo;
 	private List<String> retrets = new ArrayList<String>();
-	private String selectedRetret ;
-	private Retret retretvalue ;
-	private ZipCode zipID ;
-	private String zipcode ;
-	
-	
+	private String selectedRetret;
+	private Retret retretvalue;
+	private ZipCode zipID;
+	private String zipcode;
+
 	@EJB
 	private FieldIBusiness fieldIBusiness;
 	@EJB
@@ -66,60 +68,82 @@ public class FieldManagedBean implements Serializable{
 	@EJB
 	private ZipCodeIBusiness zipCodeIBusiness;
 	@EJB
-	private RetretIBusiness retretIBusiness ; 
-	
+	private RetretIBusiness retretIBusiness;
+
 	@PostConstruct
 	public void init() {
-		slopes = getAllSlopeLabels() ;
+		slopes = getAllSlopeLabels();
 		grasses = getAllGrassLabels();
-		humidities = getAllHumidityLabels() ;
-		Veggies = getAllVeggyLabels() ;
+		humidities = getAllHumidityLabels();
+		Veggies = getAllVeggyLabels();
 		compos = getAllCompoLabels();
-		retrets = getAllRetretLabels() ;
+		retrets = getAllRetretLabels();
 		zipID = getCityWithZipCodeID();
 		getCityWithZipCodeCode(zipcode);
 	}
-	
-	
+
+	public void updateField(PageManageBean pageManagedBean, Field field) {
+		selectField = field;
+		pageManagedBean.setPage("updatefield");
+
+	}
+
+	public void getFieldDetail(PageManageBean pageManagedBean, Field field) {
+		selectField = field;
+		pageManagedBean.setPage("terrain_detail");
+
+	}
+
+	public List<Field> getAllByIdAccount(int id) {
+		allField = getAllFieldByIdAccount(id);
+
+		if (selectField.getAccount() == null & allField != null) {
+			selectField = allField.get(0);
+		}
+		return allField;
+	}
+
 	public List<String> getAllSlopeLabels() {
 		return slopeIBusiness.extraireToutLesSlopesLabels();
 	}
+
 	public List<String> getAllGrassLabels() {
 		return grassIBusiness.extraireToutLesGlassHeightLabels();
 	}
+
 	public List<String> getAllHumidityLabels() {
-		return humidityIBusiness.extraireToutLesHumidityLabels() ;
+		return humidityIBusiness.extraireToutLesHumidityLabels();
 	}
+
 	public List<String> getAllVeggyLabels() {
 		return veggyIBusiness.extraireToutesLesVegetationLabels();
 	}
+
 	public List<String> getAllCompoLabels() {
 		return compoIBusiness.extraireToutesLesCompositionLabels();
 	}
-//	public String getCityWithZipCode(String zipCode) {
-//		return zipCodeIBusiness.extraireVilleAvecCodePostale(zipCode) ;
-//	}
+
 	public List<String> getAllRetretLabels() {
 		return retretIBusiness.extraireToutLesRetretLabels();
 	}
+
 	public ZipCode getCityWithZipCodeCode(String zipCode) {
-	return zipCodeIBusiness.extraireZipCodeAvecCodePostale(zipCode);
-    }	
+		return zipCodeIBusiness.extraireZipCodeAvecCodePostale(zipCode);
+	}
+
 	public ZipCode getCityWithZipCodeID() {
 		return zipCodeIBusiness.extraireIncidentParId(20225);
-	}	
+	}
 
-	
-	
 	public String supprimer(List<Retret> retretsList) {
-		
-		retretvalue = retretsList.get(Integer.parseInt(selectedRetret)-1) ;
+
+		retretvalue = retretsList.get(Integer.parseInt(selectedRetret) - 1);
 		field.setWithdraw(new Date());
 		field.setRetret(retretvalue);
 		fieldIBusiness.mettreAJourUnTerrain(field);
-		return "simpleArch.xhtml"; 
+		return "simpleArch.xhtml";
 	}
-	
+
 	public Field getField() {
 		return field;
 	}
@@ -127,14 +151,11 @@ public class FieldManagedBean implements Serializable{
 	public void setField(Field field) {
 		this.field = field;
 	}
-	
-	
+
 	public void getFieldById(int id) {
 		field = fieldIBusiness.extraireTerrainParId(id);
 	}
-	
-	
-	
+
 	public Field getFieldByIdAccount(int id) {
 		return fieldIBusiness.extraireTerrainParIdAccount(id);
 	}
@@ -142,7 +163,7 @@ public class FieldManagedBean implements Serializable{
 	public List<Field> getAllField() {
 		return fieldIBusiness.extraireToutLesTerrain();
 	}
-	
+
 	public List<Field> getAllFieldByIdAccount(int id) {
 		return fieldIBusiness.extraireToutLesTerrainParIdAccount(id);
 	}
@@ -163,133 +184,119 @@ public class FieldManagedBean implements Serializable{
 		this.selectedSlope = selectedSlope;
 	}
 
-
 	public List<String> getGrasses() {
 		return grasses;
 	}
-
 
 	public void setGrasses(List<String> grasses) {
 		this.grasses = grasses;
 	}
 
-
 	public String getSelectedGrass() {
 		return selectedGrass;
 	}
-
 
 	public void setSelectedGrass(String selectedGrass) {
 		this.selectedGrass = selectedGrass;
 	}
 
-
 	public String getSelectedHumidity() {
 		return selectedHumidity;
 	}
-
 
 	public void setSelectedHumidity(String selectedHumidity) {
 		this.selectedHumidity = selectedHumidity;
 	}
 
-
 	public List<String> getVeggies() {
 		return Veggies;
 	}
-
 
 	public void setVeggies(List<String> veggies) {
 		Veggies = veggies;
 	}
 
-
 	public String getSelectedVeggy() {
 		return selectedVeggy;
 	}
-
 
 	public void setSelectedVeggy(String selectedVeggy) {
 		this.selectedVeggy = selectedVeggy;
 	}
 
-
 	public List<String> getCompos() {
 		return compos;
 	}
-
 
 	public void setCompos(List<String> compos) {
 		this.compos = compos;
 	}
 
-
 	public String getSelectedCompo() {
 		return selectedCompo;
 	}
-
 
 	public void setSelectedCompo(String selectedCompo) {
 		this.selectedCompo = selectedCompo;
 	}
 
-
 	public List<String> getHumidities() {
 		return humidities;
 	}
-
 
 	public void setHumidities(List<String> humidities) {
 		this.humidities = humidities;
 	}
 
-
 	public List<String> getRetrets() {
 		return retrets;
 	}
-
 
 	public void setRetrets(List<String> retrets) {
 		this.retrets = retrets;
 	}
 
-
 	public String getSelectedRetret() {
 		return selectedRetret;
 	}
-
 
 	public void setSelectedRetret(String selectedRetret) {
 		this.selectedRetret = selectedRetret;
 	}
 
-
 	public Retret getRetretvalue() {
 		return retretvalue;
 	}
-
 
 	public void setRetretvalue(Retret retretvalue) {
 		this.retretvalue = retretvalue;
 	}
 
-
 	public ZipCode getZipID() {
 		return zipID;
 	}
-
 
 	public void setZipID(ZipCode zipID) {
 		this.zipID = zipID;
 	}
 
-
 	public String getZipcode() {
 		return zipcode;
 	}
 
-
 	public void setZipcode(String zipcode) {
 		this.zipcode = zipcode;
+	}
+
+	public Field getSelectField() {
+		return selectField;
+	}
+
+	public void setSelectField(Field selectField) {
+		this.selectField = selectField;
+	}
+
+	public void setAllField(List<Field> allField) {
+		this.allField = allField;
 	}
 }
--- AppTontaPatWeb/src/main/java/fr/eql/ai109/apptontapat/controller/HerdManagedBean.java
+++ AppTontaPatWeb/src/main/java/fr/eql/ai109/apptontapat/controller/HerdManagedBean.java
@@ -27,26 +27,27 @@ public class HerdManagedBean implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private Herd herd = new Herd();
 	private String test;
 	private List<String> races = new ArrayList<String>();
-	private String selectedRace ;
+	private String selectedRace;
 	private List<String> species = new ArrayList<String>();
 	private String selectedSpecie;
 	private List<String> takeouts = new ArrayList<String>();
-	private String selectedTakeout ;
-	private TakeOut takeoutvalue ; 
-	
-	
+	private String selectedTakeout;
+	private TakeOut takeoutvalue;
+	private Herd SelectHerd = new Herd();
+
 	@PostConstruct
 	public void init() {
-		races = getAllRaceLabels(); 
+		races = getAllRaceLabels();
 		species = getAllSpecieLabels();
 		takeouts = getAllTakeoutLabels();
 		
+
 	}
-	
+
 	@EJB
 	private HerdIbusiness herdIBusiness;
 	@EJB
@@ -56,57 +57,24 @@ public class HerdManagedBean implements Serializable {
 	@EJB
 	private ZipCodeIBusiness zipCodeIBusiness;
 	@EJB
-	private TakeoutIBusiness takeoutIBusiness ;
-	
-	
-	public List<String> getAllRaceLabels() {
-		return raceIBusiness.extraireTouteLesracesLabels();
-	}
-	public List<String> getAllSpecieLabels() {
-		return specieIBusiness.extraireToutLesLabelsSpecies() ;
-	}
-	public List<String> getAllTakeoutLabels() {
-		return takeoutIBusiness.extraireToutesLesTakeoutLabels();
-	}
-	
-	
-	
+	private TakeoutIBusiness takeoutIBusiness;
 
-	public Herd getHerdByIdAccount(int id) {
-		return herdIBusiness.extraireTroupeauParIdAccount(id);
-	}
-	public List<Herd> getAllHerd() {
-		return herdIBusiness.extraireToutLesTroupeaux();
-	}
-	public List<Herd> getAllHerdByIdAccount(int id) {
-		return herdIBusiness.extraireToutLesTroupeauxParIdAccount(id);
-	}
-	public String getCityWithZipCode(String zipCode) {
-		return zipCodeIBusiness.extraireVilleAvecCodePostale(zipCode) ;
-	}
-	
+	public String supprimer(List<TakeOut> takeoutsList) {
 
-	
-public String supprimer(List<TakeOut> takeoutsList) {
-		
-		takeoutvalue = takeoutsList.get(Integer.parseInt(selectedTakeout)-1) ;
+		takeoutvalue = takeoutsList.get(Integer.parseInt(selectedTakeout) - 1);
 		System.out.println("valeur de takeoutvalue ----------------------------" + takeoutvalue);
-		
+
 		System.out.println("valeur de selectedTakeout ----------------------------" + selectedTakeout);
 		herd.setWithdraw(new Date());
 		herd.setTakeout(takeoutvalue);
 		herdIBusiness.mettreAJourUnTroupeau(herd);
-		return "simpleArch.xhtml"; 
+		return "simpleArch.xhtml";
 	}
 
-
-	
 	public void getHerdById(int id) {
-		System.out.println("test d'apelle de function get test et id: "+id);
-		herd=herdIBusiness.extraireTroupeauParId(id);
-		
-//		System.out.println(" test1 methode "+ getCityWithZipCode("24252"));
-//		System.out.println(" test2: " + herd.getZipcode().getLabel());
+		System.out.println("test d'apelle de function get test et id: " + id);
+		herd = herdIBusiness.extraireTroupeauParId(id);
+
 	}
 
 	public Herd getHerd() {
@@ -116,10 +84,9 @@ public String supprimer(List<TakeOut> takeoutsList) {
 	public void setHerd(Herd herd) {
 		this.herd = herd;
 	}
-	
 
 	public String getTest(int id) {
-		System.out.println("test d'apelle de function get test et id: "+id);
+		System.out.println("test d'apelle de function get test et id: " + id);
 		return test;
 	}
 
@@ -154,29 +121,65 @@ public String supprimer(List<TakeOut> takeoutsList) {
 	public void setSelectedSpecie(String selectedSpecie) {
 		this.selectedSpecie = selectedSpecie;
 	}
+
 	public List<String> getTakeouts() {
 		return takeouts;
 	}
+
 	public void setTakeouts(List<String> takeouts) {
 		this.takeouts = takeouts;
 	}
+
 	public String getSelectedTakeout() {
 		return selectedTakeout;
 	}
+
 	public void setSelectedTakeout(String selectedTakeout) {
 		this.selectedTakeout = selectedTakeout;
 	}
+
 	public TakeOut getTakeoutvalue() {
 		return takeoutvalue;
 	}
+
 	public void setTakeoutvalue(TakeOut takeoutvalue) {
 		this.takeoutvalue = takeoutvalue;
 	}
-	
 
-	
-	
-	
+	public Herd getSelectHerd() {
+		return SelectHerd;
+	}
 
+	public void setSelectHerd(Herd selectHerd) {
+		SelectHerd = selectHerd;
+	}
+
+	public List<String> getAllRaceLabels() {
+		return raceIBusiness.extraireTouteLesracesLabels();
+	}
+
+	public List<String> getAllSpecieLabels() {
+		return specieIBusiness.extraireToutLesLabelsSpecies();
+	}
+
+	public List<String> getAllTakeoutLabels() {
+		return takeoutIBusiness.extraireToutesLesTakeoutLabels();
+	}
+
+	public Herd getHerdByIdAccount(int id) {
+		return herdIBusiness.extraireTroupeauParIdAccount(id);
+	}
+
+	public List<Herd> getAllHerd() {
+		return herdIBusiness.extraireToutLesTroupeaux();
+	}
+
+	public List<Herd> getAllHerdByIdAccount(int id) {
+		return herdIBusiness.extraireToutLesTroupeauxParIdAccount(id);
+	}
+
+	public String getCityWithZipCode(String zipCode) {
+		return zipCodeIBusiness.extraireVilleAvecCodePostale(zipCode);
+	}
 
 }
